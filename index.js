27a96b999e8706e0c0a95f00c7890583060e108f
Jonster5 <onlyaccelerating@gmail.com>
2019-10-30 12:57:55 +0000

    thing

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 
+let PORT = process.env.PORT;
 const express = require('express');
 let app = express();
 
 app.get('/pebble2d', (req, res) => {
     res.download('Pebble2d/Pebble2d.js');
 });
+
+app.listen(PORT);
